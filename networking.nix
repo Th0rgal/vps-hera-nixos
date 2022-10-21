b73eb1f4d0281ec32df6ba2970293d7bffbef7b3
Thomas Marchand <thomas.marchand@tuta.io>
2022-10-21 12:20:02 +0200

    feat: update nameservers

--- networking.nix
+++ networking.nix
@@ -8,10 +8,12 @@
 
   networking = {
     hostName = "hera";
+    nameservers = [ "1.1.1.1" "9.9.9.9" ];
     firewall = {
       enable = false;
       allowPing = true;
-      allowedTCPPorts = [ 80 443 6000 25565 8146 5432 ]; # HTTP, SSL, TOR, MINECRAFT, SQL
+      allowedTCPPorts =
+        [ 80 443 6000 25565 8146 5432 ]; # HTTP, SSL, TOR, MINECRAFT, SQL
     };
   };
 }
